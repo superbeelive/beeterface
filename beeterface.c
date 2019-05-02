cad771c54a80afd8dd44bf653626fb1219c4457a
Olivia SP <olivia.serenelli.pesin@gmail.com>
2019-05-02 13:52:52 +0200

    Modifications interface principale

--- beeterface.c
+++ beeterface.c
@@ -118,20 +118,30 @@ int main (int argc, char *argv[])
     GtkWidget *image_button_quit;
     GtkWidget *box_up; //Boite la plus haute pour le bouton quitter (provisoire ?) 
     GtkWidget *box_all; //Reste de la zone 
+    GtkWidget *box_info_video;
     GtkWidget *image_exemple; //Image pour repérer où sera la vidéo
     GtkWidget *box_menu_droit; //Boite pour la droite de la vidéo
     GtkWidget *box_gauche; //Boite contenant vidéo & menus
     GtkWidget *box_video; //boite avec la vidéo + nom de la vidéo
     GtkWidget *box_menu_video; //Menu sous la vidéo
+    GtkWidget *box_quit;
+    GtkWidget *box_search_1;
+    GtkWidget *box_search_2;
     GtkWidget *sep; //Separateur vertical  entre image et selecteur fichier
     GtkWidget *fichiers; 
-    GtkWidget *text;
+    GtkWidget *search;
     GtkWidget *nom_video;
     GtkWidget *choix_camera;
     GtkWidget *button_enregistrer;
+    GtkWidget *image_button_enregistrer;
     GtkWidget *button_stop;
+    GtkWidget *image_button_stop;
     GtkWidget *button_photo;
     GtkWidget *image_button_photo;
+    GtkWidget *logo_type_video;
+    GtkWidget *choix_tri;
+    GtkWidget *txt_trier_par;
+    GtkWidget *txt_visualiser;
 
     gtk_init (&argc, &argv);
 
@@ -146,7 +156,11 @@ int main (int argc, char *argv[])
     box_gauche = gtk_box_new( GTK_ORIENTATION_VERTICAL, 0);
     box_video = gtk_box_new( GTK_ORIENTATION_VERTICAL, 0);
     box_menu_video = gtk_box_new( GTK_ORIENTATION_HORIZONTAL, 0);
-
+    box_info_video = gtk_box_new( GTK_ORIENTATION_HORIZONTAL, 0);
+    box_quit = gtk_box_new( GTK_ORIENTATION_VERTICAL, 0);
+    box_search_1 = gtk_box_new( GTK_ORIENTATION_HORIZONTAL, 0);
+    box_search_2 = gtk_box_new( GTK_ORIENTATION_VERTICAL, 0);
+    
 
     //BOUTONS
     button_quit = gtk_button_new (); //Definition sur deux lignes  
@@ -154,30 +168,49 @@ int main (int argc, char *argv[])
     image_button_quit = gtk_image_new_from_file ("images/quit3.png");
     gtk_button_set_image (GTK_BUTTON (button_quit), image_button_quit);
 
-    button_enregistrer = gtk_button_new_with_label ("Enregistrer");
-    button_stop = gtk_button_new_with_label ("Stop");
-        //button_photo_with_image
+    button_enregistrer = gtk_button_new ();
+    image_button_enregistrer = gtk_image_new_from_file ("images/enregistrer.png");
+    gtk_button_set_image (GTK_BUTTON (button_enregistrer), image_button_enregistrer);
+
+
+    button_stop = gtk_button_new ();
+    image_button_stop =  gtk_image_new_from_file ("images/carre_noir.png");
+    gtk_button_set_image (GTK_BUTTON (button_stop), image_button_stop);
+
     button_photo = gtk_button_new ();
     image_button_photo = gtk_image_new_from_file ("images/icone_photo3.png");
     gtk_button_set_image (GTK_BUTTON (button_photo), image_button_photo);
 
     //IMAGES  
     image_exemple = gtk_image_new_from_file ("images/bee2.jpg");
-
+    logo_type_video = gtk_image_new_from_file ("images/cassette2.png");
     //TEXTE 
-    nom_video = gtk_label_new("Nom de la vidéo");
+    nom_video = gtk_label_new("");
+    gtk_label_set_markup(GTK_LABEL(nom_video), "<span foreground=\"black\" font=\"16\">Nom de la vidéo</span>");
+    
+    txt_trier_par = gtk_label_new("");
+    gtk_label_set_markup(GTK_LABEL(txt_trier_par), "<span foreground=\"black\" font=\"10\">Trier par :</span>");
+
+    txt_visualiser = gtk_label_new("");
+    gtk_label_set_markup(GTK_LABEL(txt_visualiser), "<span foreground=\"black\" font=\"10\">Visualiser :</span>");
+
+
 
     //AUTRES 
     fichiers = gtk_places_sidebar_new ();
     sep = gtk_separator_new(GTK_ORIENTATION_VERTICAL);
-    text = gtk_entry_new ();
+    search = gtk_search_entry_new ();
     
     choix_camera = gtk_combo_box_text_new();
     gtk_combo_box_text_insert_text ((GtkComboBoxText*) choix_camera, 1,"Caméra n°1");
     gtk_combo_box_text_insert_text ((GtkComboBoxText*) choix_camera, 2,"Caméra n°2");
     gtk_combo_box_text_insert_text ((GtkComboBoxText*) choix_camera, 3,"Caméra n°3");
-//Affichage d'un modele 
 
+    choix_tri =  gtk_combo_box_text_new();
+    gtk_combo_box_text_insert_text ((GtkComboBoxText*) choix_tri, 3,"Date");
+    gtk_combo_box_text_insert_text ((GtkComboBoxText*) choix_tri, 3,"Nom");
+    gtk_combo_box_text_insert_text ((GtkComboBoxText*) choix_tri, 3,"Tag");
+    gtk_combo_box_text_insert_text ((GtkComboBoxText*) choix_tri, 3,"Caméra");
 
 
 //Rangement des boxs & Widgets 
@@ -189,23 +222,40 @@ int main (int argc, char *argv[])
         gtk_box_pack_start(GTK_BOX(box_principal), box_all, TRUE, TRUE, 0);
 
             //Dans box_up
-            gtk_box_pack_start(GTK_BOX(box_up), button_quit, TRUE, TRUE, 0);
-       
+            gtk_box_pack_start(GTK_BOX(box_up), box_quit, TRUE, TRUE, 0); 
+            gtk_box_pack_start(GTK_BOX(box_up), box_info_video, TRUE, TRUE, 0); 
+                 //Dans box_info_video   
+                 gtk_box_pack_start(GTK_BOX(box_info_video), logo_type_video, FALSE, FALSE, 20);
+                 gtk_box_pack_start(GTK_BOX(box_info_video), nom_video, FALSE, FALSE, 0);
+
+                 //Dans box_quit
+                 gtk_box_pack_start(GTK_BOX(box_quit), button_quit, FALSE, FALSE, 0);
             //Dans box_all
             gtk_box_pack_start(GTK_BOX(box_all), box_gauche, FALSE, FALSE, 0);
             gtk_box_pack_start(GTK_BOX(box_all), sep, FALSE, FALSE, 0);
             gtk_box_pack_start(GTK_BOX(box_all), box_menu_droit, TRUE, TRUE, 0);
+
                 //Dans box_menu_droit
-                 gtk_box_pack_start(GTK_BOX(box_menu_droit), fichiers, FALSE, FALSE, 0);
-                 gtk_box_pack_start(GTK_BOX(box_menu_droit), text, FALSE, FALSE, 0);
+                 gtk_box_pack_start(GTK_BOX(box_menu_droit), box_search_1, FALSE, FALSE, 0);
+                 gtk_box_pack_start(GTK_BOX(box_menu_droit), box_search_2, FALSE, FALSE, 0);
+                    //Dans box_search_1
+                    gtk_box_pack_start(GTK_BOX(box_search_1), search, FALSE, FALSE, 5);
+                    gtk_box_pack_start(GTK_BOX(box_search_1), txt_trier_par, FALSE, FALSE, 0);
+
+                    gtk_box_pack_start(GTK_BOX(box_search_1), choix_tri, FALSE, FALSE, 5);
+
+                    //Dans box_search_2
+                    gtk_box_pack_start(GTK_BOX(box_search_2), fichiers, FALSE, FALSE, 0);
+
                 //Dans box_gauche
                      gtk_box_pack_start(GTK_BOX(box_gauche), box_video, FALSE, FALSE, 0);
                      gtk_box_pack_start(GTK_BOX(box_gauche), box_menu_video, FALSE, FALSE, 0);
+
                         //Dans box_video
                              gtk_box_pack_start(GTK_BOX(box_video), image_exemple, FALSE, FALSE, 0);
-                             gtk_box_pack_start(GTK_BOX(box_video), nom_video, FALSE, FALSE, 0);
 
                         //Dans box_menu_video
+                             gtk_box_pack_start(GTK_BOX(box_menu_video), txt_visualiser, FALSE, FALSE, 10);
                              gtk_box_pack_start(GTK_BOX(box_menu_video), choix_camera, FALSE, FALSE, 0);
                              gtk_box_pack_start(GTK_BOX(box_menu_video), button_enregistrer, FALSE, FALSE, 0);
                              gtk_box_pack_start(GTK_BOX(box_menu_video), button_stop, FALSE, FALSE, 0);
@@ -225,8 +275,8 @@ int main (int argc, char *argv[])
 //AFFICHAGE 
   
     //BOX
-    gtk_widget_show (box_principal);
-    gtk_widget_show (box_up);
+    gtk_widget_show_all (box_principal);
+    gtk_widget_show_all (box_up);
     gtk_widget_show (box_all);
     gtk_widget_show (box_menu_droit);
     gtk_widget_show (box_gauche);
@@ -249,7 +299,6 @@ int main (int argc, char *argv[])
     //AUTRE
     gtk_widget_show (sep);
     gtk_widget_show (fichiers);
-    gtk_widget_show (text);
     gtk_widget_show (choix_camera);
 
     //FENETRE
