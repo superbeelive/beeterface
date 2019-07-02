2f70f1082a1a93d7b72c3c059ddb799a8538c3ad
Sebastien Druon <seb@homedruon.com>
2019-07-02 22:50:10 +0200

    working on serialize.h

--- src/serialize.h
+++ src/serialize.h
@@ -3,6 +3,12 @@
 
 #include <rpc/xdr.h>
 
+#include "auteur.h"
+#include "camera.h"
+#include "video.h"
+#include "color.h"
+#include "projet.h"
+
 typedef struct {
     void *data;             // actual data
     int position;           // where shall the next data be stored or read
@@ -12,7 +18,13 @@ typedef struct {
 buffer_t* buffer_new() ;
 void buffer_del ( buffer_t*) ;
 
-void xdrbuffer_create( XDR* xdrs, buffer_t* buffer, enum xdr_op op ) ;
-void xdrbuffer_destroy( XDR* xdrs ) ;
+void xdrbuffer_create( XDR*, buffer_t*, enum xdr_op ) ;
+void xdrbuffer_destroy( XDR* ) ;
+
+int xdr_auteur( XDR*, auteur_t* ) ;
+int xdr_camera( XDR*, camera_t* ) ;
+int xdr_video( XDR*, video_t* ) ;
+int xdr_color( XDR*, color_t* ) ;
+int xdr_projet( XDR*, projet_t* ) ;
 
 #endif
