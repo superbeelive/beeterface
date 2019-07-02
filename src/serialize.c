cd7fcdeb234686c7ae13753edb92b2a43a5e1828
Sebastien Druon <seb@homedruon.com>
2019-07-02 22:20:14 +0200

    implemented xdrbuffer_create / destroy

--- src/serialize.c
+++ src/serialize.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <rpc/xdr.h>
 
 #include "utils.h"
 #include "serialize.h"
 
-#define INITIAL_BUFFER_SIZE 128 
+#define INITIAL_BUFFER_SIZE 1024 
 
 
 void buffer_reserve_space( buffer_t* b, size_t bytes ) ;
+int buffer_read( void* iohandle, void* buf, int nbytes ) ;
+int buffer_write( void* iohandle, void* buf, int nbytes ) ;
+
 
 
 buffer_t* buffer_new() {
@@ -28,7 +30,7 @@ buffer_t* buffer_new() {
     }
 
     b->size = INITIAL_BUFFER_SIZE;
-    b->next = 0;
+    b->position = 0;
 
     return b ;
 }
@@ -41,20 +43,41 @@ void buffer_del ( buffer_t* b) {
 
 
 void buffer_reserve_space( buffer_t* b, size_t bytes ) {
-    if ( (b->next + bytes) > ( b->size )) {
+    if ( (b->position + bytes) > ( b->size )) {
         b->data = realloc( b->data, b->size * 2 ) ;
         b->size *= 2 ;
     }
 }
 
 
-int buffer_write( char* iohandle, char* buf, int nbytes ) {
+int buffer_write( void* iohandle, void* buf, int nbytes ) {
     buffer_t* b = (buffer_t*) iohandle ;
     buffer_reserve_space( b, nbytes ) ;
-    memcpy((char*)b->data + n->next, buf, nbytes ) ;
-    b->next += sizeof(int) ;
+    memcpy(b->data + b->position, buf, nbytes ) ;
+    b->position += nbytes ;
     return nbytes ;                         // FIXME
 }
 
+int buffer_read( void* iohandle, void* buf, int nbytes ) {
+    int actual_read_size ;
+    buffer_t* b = (buffer_t*) iohandle ;
+    actual_read_size = MIN( nbytes, b->size - b->position ) ;
+    memcpy( buf, b->data + b->position, actual_read_size ) ;
+    b->position += actual_read_size ;
+    return actual_read_size ;
+}
 
+void xdrbuffer_create( XDR* xdrs, buffer_t* buffer, enum xdr_op op ) {
+    xdrrec_create( xdrs, 0, 0, (char*) buffer, &buffer_read, &buffer_write ) ;
+    xdrs->x_op = op ;
+    if ( op == XDR_DECODE ) {
+        xdrrec_skiprecord( xdrs ) ;
+    }
+}
 
+void xdrbuffer_destroy( XDR* xdrs ) {
+    if ( xdrs->x_op == XDR_ENCODE ) {
+    xdrrec_endofrecord( xdrs, 1) ;
+    }
+    xdr_destroy( xdrs ) ;
+}
--- src/serialize.h
+++ src/serialize.h
@@ -1,17 +1,18 @@
 #ifndef SERIALIZE_H
 #define SERIALIZE_H
 
-
+#include <rpc/xdr.h>
 
 typedef struct {
     void *data;             // actual data
-    int next;               // where shall the next data be stored
+    int position;           // where shall the next data be stored or read
     size_t size;            // size of allocated memory
 } buffer_t ;
 
 buffer_t* buffer_new() ;
 void buffer_del ( buffer_t*) ;
 
-
+void xdrbuffer_create( XDR* xdrs, buffer_t* buffer, enum xdr_op op ) ;
+void xdrbuffer_destroy( XDR* xdrs ) ;
 
 #endif
