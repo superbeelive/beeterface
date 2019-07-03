1af9049ad7185e4924c7f5dcb5e39fb87d65c704
Sebastien Druon <seb@homedruon.com>
2019-07-03 11:05:22 +0200

    working on projet_t

--- src/projet.c
+++ src/projet.c
@@ -2,31 +2,71 @@
 #include <stdlib.h>
 
 #include "projet.h"
+#include "utils.h"
 
-#define TAB_TAILLE 128
+#define TAB_SIZE 128
 
 
 projet_t* projet_new() { 
     
     projet_t* ficus ;
     ficus = malloc ( sizeof(projet_t) ) ;
-    ficus->auteur = auteur_new() ; 
-    ficus->camera = camera_new() ; 
-    ficus->video = video_new() ; 
-    ficus->oblong = malloc ( TAB_TAILLE* sizeof(oblong_t) ) ; 
-    ficus->cross = malloc ( TAB_TAILLE * sizeof(cross_t) ) ; 
-    ficus->arrow = malloc ( TAB_TAILLE * sizeof(arrow_t) ) ; 
-    ficus->text = malloc ( TAB_TAILLE * sizeof(text_t) ) ; 
-    ficus->measure = malloc ( TAB_TAILLE * sizeof(measure_t) ) ; 
-    ficus->moving_cross = malloc ( TAB_TAILLE* sizeof(moving_cross_t)) ; 
+    ficus->camera = NULL ; 
+    ficus->video = NULL ; 
     
+    ficus->oblong = malloc ( TAB_SIZE* sizeof(oblong_t*) ) ;
+    if ( ficus->oblong == NULL ) {
+       throw_error("malloc failed", 1 ) ;
+       return NULL ;
+    }
+    ficus->oblong_size = TAB_SIZE ;
+    ficus->oblong_n = 0 ;
+
+    ficus->cross = malloc ( TAB_SIZE* sizeof(cross_t*) ) ;
+    if ( ficus->cross == NULL ) {
+       throw_error("malloc failed", 1 ) ;
+       return NULL ;
+    }
+    ficus->cross_size = TAB_SIZE ;
+    ficus->cross_n = 0 ;
+    
+    ficus->arrow = malloc ( TAB_SIZE* sizeof(arrow_t*) ) ;
+    if ( ficus->arrow == NULL ) {
+       throw_error("malloc failed", 1 ) ;
+       return NULL ;
+    }
+    ficus->arrow_size = TAB_SIZE ;
+    ficus->arrow_n = 0 ;
+
+    ficus->text = malloc ( TAB_SIZE* sizeof(text_t*) ) ;
+    if ( ficus->text == NULL ) {
+       throw_error("malloc failed", 1 ) ;
+       return NULL ;
+    }
+    ficus->text_size = TAB_SIZE ;
+    ficus->text_n = 0 ;
+
+    ficus->measure = malloc ( TAB_SIZE* sizeof(measure_t*) ) ;
+    if ( ficus->measure == NULL ) {
+       throw_error("malloc failed", 1 ) ;
+       return NULL ;
+    }
+    ficus->measure_size = TAB_SIZE ;
+    ficus->measure_n = 0 ;
+
+    ficus->moving_cross = malloc ( TAB_SIZE* sizeof(moving_cross_t*) ) ;
+    if ( ficus->moving_cross == NULL ) {
+       throw_error("malloc failed", 1 ) ;
+       return NULL ;
+    }
+    ficus->moving_cross_size = TAB_SIZE ;
+    ficus->moving_cross_n = 0 ;
+
     return ficus ; 
 }
 
 void projet_del (projet_t* ficus ) {
-    auteur_del ( ficus->auteur ) ; 
-    camera_del ( ficus->camera ) ;
-    video_del  ( ficus->video ) ; 
+    projet_flush(ficus) ;
     free ( ficus->oblong ) ; 
     free ( ficus->cross ) ; 
     free ( ficus->arrow ) ; 
@@ -36,4 +76,50 @@ void projet_del (projet_t* ficus ) {
     free ( ficus ) ; 
 }
 
+void projet_flush( projet_t* ficus) {
+    int i ;
+    
+    if (ficus->camera != NULL ) 
+        camera_del ( ficus->camera ) ;
 
+    if (ficus->video != NULL ) 
+        video_del  ( ficus->video ) ; 
+
+    for (i=0; i<ficus->oblong_n; i++)
+        free(ficus->oblong[i]) ;
+
+    for (i=0; i<ficus->cross_n; i++)
+        free(ficus->cross[i]) ;
+
+    for (i=0; i<ficus->arrow_n; i++)
+        free(ficus->arrow[i]) ;
+    
+    for (i=0; i<ficus->text_n; i++)
+        free(ficus->text[i]) ;
+    
+    for (i=0; i<ficus->measure_n; i++)
+        free(ficus->measure[i]) ;
+    
+    for (i=0; i<ficus->moving_cross_n; i++)
+        free(ficus->moving_cross[i]) ;
+
+    ficus->oblong_n = 0 ;
+    ficus->cross_n = 0 ;
+    ficus->arrow_n = 0 ;
+    ficus->text_n = 0 ;
+    ficus->measure_n = 0 ;
+    ficus->moving_cross_n = 0 ;
+
+    ficus->camera = NULL ;
+    ficus->video = NULL ;
+
+}
+
+void projet_add_cross( projet_t* projet, cross_t* cross ) {
+    if (projet->cross_n == projet->cross_size ) {
+        projet->cross = reallocarray( projet->cross, projet->cross_size+TAB_SIZE, sizeof(cross_t*) ) ;
+        projet->cross_size += TAB_SIZE ;
+    }
+    projet->cross[projet->cross_n] = cross ;
+    projet->cross_n +=1 ;
+}
--- src/projet.h
+++ src/projet.h
@@ -8,22 +8,41 @@
 #include "auteur.h"
 
 typedef struct {
-    
-    auteur_t* auteur ; 
+   
     camera_t* camera ; 
     video_t* video ;
     
-    oblong_t* oblong ; 	// tableau
-    cross_t* cross ; 	// tableau
-    arrow_t* arrow ; 
-    text_t* text ; 
-    measure_t* measure ; 
-    moving_cross_t* moving_cross ; 
+    oblong_t** oblong ; 	        // tableau
+    int oblong_n ;              // Nombre d'objects stockés
+    int oblong_size ;           // Tqille du tableau
+
+    cross_t** cross ;
+    int cross_n ;
+    int cross_size ;
+
+
+    arrow_t** arrow ;
+    int arrow_n ;
+    int arrow_size ;
+
+    text_t** text ; 
+    int text_n ;
+    int text_size ;
+
+    measure_t** measure ; 
+    int measure_n ;
+    int measure_size ;
+
+    moving_cross_t** moving_cross ; 
+    int moving_cross_n ;
+    int moving_cross_size ;
     
 
 } projet_t ; 
 
 projet_t* projet_new() ; 
 void projet_del (projet_t*) ; 
+void projet_flush( projet_t*) ;
 
+void projet_add_cross( projet_t*, cross_t*); 
 #endif 
