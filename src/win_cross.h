b979fb124f377a1a57fdf1e1ce78b7770322c3ac
Olivia SP <olivia.serenelli.pesin@gmail.com>
2019-07-03 16:37:33 +0200

    Modif win cross

--- src/win_cross.h
+++ src/win_cross.h
@@ -13,6 +13,7 @@ typedef struct {
     
     GtkWidget* box_name ;
     GtkWidget* box_description ;
+    GtkWidget* box_video ; 
     GtkWidget* box_time ; 
     GtkWidget* box_label_time ; 
     GtkWidget* box_btn_video ; 
@@ -20,13 +21,73 @@ typedef struct {
     GtkWidget* box_def_time_fin ; 
 
     GtkWidget* box_tag ; 
-    GtkWidget* box_info ; 
+    GtkWidget* box_txt_tag ; 
+    GtkWidget* box_scroll_tag ; 
+    GtkWidget* box_meta ; 
+    GtkWidget* box_text_meta ; 
     GtkWidget* box_btn_quit ;
-
-    //IMG
     
-    //LABEL
+    //frame
+    GtkWidget* frame_tag ; 
+    GtkWidget* frame_meta ; 
 
+    //scroll
+    GtkWidget* scroll_tag ;
+
+    //GRID 
+    GtkWidget* grid_meta ; 
+    //IMG
+    GtkWidget* img_ex_video ; 
+
+    //LABEL
+    GtkWidget* label_name ; 
+    GtkWidget* label_desc ;
+    GtkWidget* label_time_deb ; 
+    GtkWidget* label_time_fin ;
+
+    GtkWidget* label_test ;
+
+    GtkWidget* label_name_ruche ;
+    GtkWidget* label_description ;
+    GtkWidget* label_camera ;
+    GtkWidget* label_nruche ;
+    GtkWidget* label_ncadre ;
+
+    GtkWidget* label_info_name_ruche ;
+    GtkWidget* label_info_description ;
+    GtkWidget* label_info_camera ;
+    GtkWidget* label_info_nruche ;
+    GtkWidget* label_info_ncadre ;
+
+    GtkWidget* label_enr_timed ; 
+    GtkWidget* label_enr_timef ; 
+    GtkWidget* label_timed ;
+    GtkWidget* label_timef ;
+
+    //ENTRY 
+    GtkWidget* entry_name ; 
+
+    //BTN
+    GtkWidget* btn_name ; 
+    GtkWidget* img_btn_ok_name ;
+    GtkWidget* img_btn_mod_name ;
+
+    GtkWidget* btn_cut ;
+    GtkWidget* btn_play ;
+    GtkWidget* btn_av ;
+    GtkWidget* btn_rec ;
+
+    GtkWidget* btn_enr ;
+    GtkWidget* btn_ann ; 
+    
+    GtkWidget* btn_enr_timed ;
+    GtkWidget* btn_enr_timef ;
+    //OTHER
+    GtkTextBuffer* buffer_description ; 
+    GtkWidget* text_description ;
+
+    GtkWidget* scale_time ; 
+    GtkAdjustment* adj_time ; 
 
 } cross_win_t ;
 
